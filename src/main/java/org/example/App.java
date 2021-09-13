ea868cdf54520936abd8a0d9ad9c57763c8393a5
CailaC <caila.coates2010@gmail.com>
2021-09-13 02:21:05 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,30 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Caila Coates
+ */
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+    //p = principle, r = rate of interest, t = time/years, a = amount at end of investment
+        System.out.println( "Enter the principle: " );
+        double p = input.nextInt();
+
+        System.out.println( "Enter the rate of interest: " );
+        double r = input.nextDouble();
+        r = r/100;
+
+        System.out.println( "Enter the number of years: " );
+        double t = input.nextInt();
+
+        double a = p * (1 + (r*t));
+
+        System.out.printf("After %.0f years at %.2f, the investment will be worth $%.2f.", t, r*100, a);
+    }
+}
